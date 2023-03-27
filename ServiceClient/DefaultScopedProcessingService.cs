51931441840196c8d57ce9a5e44993128bcceabd
humberto <humberto@HUMBERTO-OFFICE>
2023-03-27 10:41:16 -0300

    Add project files.

---
+++ ServiceClient/DefaultScopedProcessingService.cs
@@ -0,0 +1,48 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace ServiceClient;
+
+public sealed class DefaultScopedProcessingService : IScopedProcessingService
+{
+    private int _executionCount;
+    private readonly ILogger<DefaultScopedProcessingService> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+    public DefaultScopedProcessingService(
+        ILogger<DefaultScopedProcessingService> logger, IServiceProvider provider)
+    {
+        _logger = logger;
+        _serviceProvider = provider;
+    }
+
+    public async Task DoWorkAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            ++ _executionCount;
+
+            // Disable the comment below and it works
+            //await Task.Delay(2000, stoppingToken);
+
+            try
+            {
+                _logger.LogInformation(
+                    "{ServiceName} working, execution count: {Count}",
+                    nameof(DefaultScopedProcessingService),
+                    _executionCount);
+                var client = _serviceProvider.GetRequiredService<IRequestClient<ServiceManagerRequest>>();
+
+                var response =
+                    await client.GetResponse<ServiceManagerResult>(
+                        new ServiceManagerRequest { Action = $"OK - {DateTime.Now}" }, stoppingToken);
+
+                await Task.Delay(1000, stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ ServiceClient/Program.cs
@@ -0,0 +1,31 @@
+using Contracts;
+using MassTransit;
+using ServiceClient;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.SetKebabCaseEndpointNameFormatter();
+    x.AddRequestClient<ServiceManagerRequest>(TimeSpan.FromSeconds(10));
+
+    x.UsingGrpc((context, cfg) =>
+    {
+        cfg.UseMessageRetry(r => r.Incremental(5, TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(500) ));
+        cfg.Host(h =>
+        {
+            h.Host = "127.0.0.1";
+            h.Port = 19797;
+
+            h.AddServer(new Uri("http://127.0.0.1:19796"));
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddHostedService<Worker>();
+builder.Services.AddScoped<IScopedProcessingService, DefaultScopedProcessingService>();
+
+var host = builder.Build();
+host.Run();
