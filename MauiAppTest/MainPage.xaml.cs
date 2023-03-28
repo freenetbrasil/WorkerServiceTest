eb10ea9a5960944d01e6688a71ca428f235e99b2
Humberto Aicardi <humberto@freenet.com.br>
2023-03-28 18:06:28 -0300

    MAUI Changes

---
+++ MauiAppTest/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Android.OS;
+using Contracts;
+using MassTransit;
+
+namespace MauiAppTest
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        private readonly IServiceProvider _provider;
+
+        public MainPage(IServiceProvider provider)
+        {
+            _provider = provider;
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = _provider.GetRequiredService<IRequestClient<ServiceManagerRequest>>();
+                var result = await client.GetResponse<ServiceManagerResult>(new ServiceManagerRequest
+                    { Action = $"OK - {DateTime.Now}" });
+            }
+            catch (Exception ex)
+            {
+                CounterBtn.Text = ex.Message;
+                return;
+            }
+
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
---
+++ MauiAppTest/MauiProgram.cs
@@ -0,0 +1,48 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace MauiAppTest
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+            builder.Services.AddMassTransit(x =>
+            {
+                x.SetKebabCaseEndpointNameFormatter();
+                x.AddRequestClient<ServiceManagerRequest>(TimeSpan.FromSeconds(10));
+
+                x.UsingGrpc((context, cfg) =>
+                {
+                    cfg.UseMessageRetry(r => r.Incremental(5, TimeSpan.FromMilliseconds(500), TimeSpan.FromMilliseconds(500)));
+                    cfg.Host(h =>
+                    {
+                        h.Host = "127.0.0.1";
+                        h.Port = 19797;
+
+                        h.AddServer(new Uri("http://127.0.0.1:19796"));
+                    });
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            builder.Services.AddSingleton<MainPage>();
+
+            return builder.Build();
+        }
+    }
+}
